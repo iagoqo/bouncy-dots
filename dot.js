de7d7e595c6d2ea96ae613a74a6826274e15489b
Iago <iagoqo@users.noreply.github.com>
2016-06-14 22:37:58 +0200

    Make dots bounce off all edges, improve documentation

--- dot.js
+++ dot.js
@@ -1,17 +1,36 @@
+/**
+ * Constructor for Dots
+ * @param {Object} attr Object with the properties of the Dot. Unknown properties will be ignored.
+ */
 function Dot(attr) {
   if (!attr) attr = {};
+  // Starting position
   this.x = attr.x || 0;
   this.y = attr.y || 0;
+
+  // Velocities, how fast the dot will move in each axis
   this.vx = attr.vx || 1;
   this.vy = attr.vy || 0;
+
+  // Radius, defines how big the dot will be
   this.radius = attr.radius || 5;
+
+  // Bounce factor, the higher this is the more the dot will bounce
   this.bounce = attr.bounce || 0.75;
+
+  // Friction factor, the lower this is the more the dot will slow down when in contact with a surface
   this.friction = attr.friction || 0.99;
+
+  // Color of the dot
   this.color = attr.color || 'grey';
 }
 
 Dot.prototype = {
 
+  /**
+   * Draws the dot on the specified canvas
+   * @param  {CanvasRenderingContext2D} ctx Context of the canvas in which to draw the dot
+   */
   draw: function(ctx) {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -20,6 +39,11 @@ Dot.prototype = {
     ctx.closePath();
   },
 
+  /**
+   * Changes the velocity and position of the dot based on the provided gravity
+   * @param  {Number} gravity Downward acceleration
+   * @return {Dot}         Current dot
+   */
   updateVelocity: function(gravity) {
     // Update velocity due to gravity
     this.vy += gravity;
@@ -27,19 +51,64 @@ Dot.prototype = {
     // Update the position of the dot
     this.y += this.vy;
     this.x += this.vx;
+
+    return this;
   },
 
-  verticalBounce: function(height) {
-    // Check if the this is at the bottom of the canvas, calculate rebound
+  /**
+   * Updates the velocity and position of a dot if it's past the top or bottom edge of the canvas
+   * @param  {Number} height Height of the canvas
+   * @return {Dot}        This dot
+   */
+  verticalEdge: function(height) {
+    // If the dot is past an edge, put it right on the edge and make it bounce
     if (this.y + this.radius > height) {
-      // Position the this just on top of the bottom of the canvas
       this.y = height - this.radius;
-
-      // The y velocity changes direction and is weaker than before
-      this.vy *= -this.bounce;
-      // Friction slows down the this on the x axis
-      this.vx *= this.friction;
+      this.verticalBounce();
+    } else if (this.y - this.radius < 0) {
+      this.y = 0 + this.radius;
+      this.verticalBounce();
     }
+
+    return this;
+  },
+
+  /**
+   * Changes the vertical direction of the dot and applies friction
+   * @return {Dot} This dot
+   */
+  verticalBounce: function() {
+    this.vy *= -this.bounce;
+    this.vx *= this.friction;
+    return this;
+  },
+
+  /**
+   * Updates the velocity and position of a dot if it's past the left or right edge of the canvas
+   * @param  {Number} width Width of the canvas
+   * @return {Dot}       This dot
+   */
+  horizontalEdge: function(width) {
+    // If the dot is past an edge, put it right on the edge and make it bounce
+    if (this.x + this.radius > width) {
+      this.x = width - this.radius;
+      this.horizontalBounce();
+    } else if (this.x - this.radius < 0) {
+      this.x = 0 + this.radius;
+      this.horizontalBounce();
+    }
+
+    return this;
+  },
+
+  /**
+   * Changes the horizontal direction of the dot and applies friction
+   * @return {Dot} This dot
+   */
+  horizontalBounce: function() {
+    this.vx *= -this.bounce;
+    this.vy *= this.friction;
+    return this;
   }
 
-}
+};
--- main.js
+++ main.js
@@ -1,52 +1,72 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+(function() {
+  'use strict';
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
 
-var gravity = 0.2;
-var dots = [];
+  console.log(canvas, ctx);
 
-canvas.onclick = function(event) {
-  var r = Math.floor(Math.random() * 256);
-  var g = Math.floor(Math.random() * 256);
-  var b = Math.floor(Math.random() * 256);
+  var gravity = 0.2;
+  var dots = [];
 
-  var attr = {
-    // Starting point = click point
-    x: event.x,
-    y: event.y,
-    // Horizontal velocity = random between -5 and 5
-    vx: Math.random() * 10 - 5,
-    // Vertical velocity = random between -10 and 5 (shoot upwards more often)
-    vy: Math.random() * 15 - 10,
-    // Radius = random between 2 and 10
-    radius: Math.random() * 8 + 2,
-    // Bounce = random between 0.05 and 0.9
-    bounce: Math.random() * 0.4 + 0.5,
-    // Friction = random between 0.95 and 1
-    friction: Math.random() * 0.05 + 0.95,
-    // Color = completely random
-    color: 'rgb(' + r + ',' + g + ',' + b + ')'
-  };
+  canvas.onclick = addDot;
 
-  dots.push(new Dot(attr));
-};
+  // Update the canvas 60 times per second
+  setInterval(draw, 1000 / 60);
 
+  /////
 
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+  /**
+   * Creates a dot at the clicked position
+   * @param  {event} event Click event
+   */
+  function addDot(event) {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
 
-function draw() {
-  clearCanvas();
-  canvas.width = canvas.offsetWidth;
-  canvas.height = canvas.offsetHeight;
-  dots.forEach(function(dot) {
-    dot.draw(ctx);
+    var attr = {
+      // Starting point = click point
+      x: event.x,
+      y: event.y,
+      // Horizontal velocity = random between -10 and 10
+      vx: Math.random() * 20 - 10,
+      // Vertical velocity = random between -15 and 5 (shoot upwards more often)
+      vy: Math.random() * 20 - 15,
+      // Radius = random between 2 and 20
+      radius: Math.random() * 18 + 2,
+      // Bounce = random between 0.05 and 0.9
+      bounce: Math.random() * 0.4 + 0.5,
+      // Friction = random between 0.95 and 0.99
+      friction: Math.random() * 0.04 + 0.95,
+      // Color = random
+      color: 'rgb(' + r + ',' + g + ',' + b + ')'
+    };
 
-    dot.updateVelocity(gravity);
+    dots.push(new Dot(attr));
+  }
 
-    dot.verticalBounce(canvas.height);
-  });
-}
+  /**
+   * Erases all the drawn objects on the canvas
+   */
+  function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
 
-// Update the canvas 60 times per second
-setInterval(draw, 1000 / 60);
+  /**
+   * Paints all the active dots on the canvas
+   */
+  function draw() {
+    clearCanvas();
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+    dots.forEach(function(dot) {
+      dot.draw(ctx);
+
+      dot
+        .updateVelocity(gravity)
+        .verticalEdge(canvas.height)
+        .horizontalEdge(canvas.width);
+    });
+  }
+
+})();
