fc8893b8a72bc0b6efe38264f480df38c277206f
Iago <iagoqo@users.noreply.github.com>
2016-06-16 20:19:46 +0200

    Add basic Gulp tasks

--- README.md
+++ README.md
@@ -2,12 +2,26 @@
 
 ## How to use
 
-- Put all the files in the same directory
-- Open the index.html file in your browser
+- Open the *index.html* file inside the *src* or *dist* directory with your browser
 
 OR
 
-- Serve the directory on a simple web server
-- Access the index.html from the URL of your server
+- Serve the *src* or *dist* directory on a simple web server
+- Access the *index.html* from the URL of your server
 
 And then just click like mad everywhere! :)
+
+
+
+## Gulp tasks
+
+Before you can run gulp tasks you need to install the required Node.js packages. To do so, run:
+
+    npm install
+
+
+### Tasks
+
+- `gulp build`: minifies the project and stores it in the *dist* directory.
+- `gulp clean`: cleans the *dist* directory
+- `gulp (default)`: gulp clean + gulp build
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+/*jslint node: true*/
+'use strict';
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var stripDebug = require('gulp-strip-debug');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+var cssmin = require('gulp-cssmin');
+var del = require('del');
+var runSequence = require('run-sequence');
+var inject = require('gulp-inject');
+var debug = require('gulp-debug');
+
+/// Config
+var dist = './dist/';
+var src = './src/';
+
+/// Tasks
+// Concats, minifies and moves CSS files to distribution folder
+gulp.task('css', function() {
+  return gulp.src(src + '**/*.css')
+    .pipe(concat('styles.min.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest(dist));
+});
+
+// Concats, minifies and moves JS files to distribution folder
+gulp.task('js', function() {
+  return gulp.src(src + '**/*.js')
+  .pipe(concat('scripts.min.js'))
+  .pipe(stripDebug())
+  .pipe(uglify())
+  .pipe(gulp.dest(dist));
+});
+
+// Injects CSS and JS files into HTML files, minifies them and moves them to the distribution folder
+gulp.task('html', ['css', 'js'], function() {
+  return gulp.src(src + '**/*.html')
+    .pipe(gulp.dest(dist))
+    .pipe(inject(gulp.src([dist+'**/*.js',dist+'**/*.css'],{read:false}),{relative:true}))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      preserveLineBreaks: true,
+      conservativeCollapse: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest(dist));
+});
+
+// Removes everything from the distribution folder
+gulp.task('clean', function() {
+  del(dist + '*');
+});
+
+// Builds the project, ready for distribution
+gulp.task('build', ['html']);
+
+// Cleans de distribution folder and builds the project
+gulp.task('default', function() {
+  runSequence('clean', 'build');
+});
